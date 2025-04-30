884dd495ba2d9c69ef77e4b47e1e449cf70822cd
Eduardo Luca <167481600+Nick2901@users.noreply.github.com>
2025-04-30 20:29:20 -0300

    Add files via upload

---
+++ filmes/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Filme
+
+def lista_filmes(request):
+    filmes = Filme.objects.all()
+    return render (request, 'filmes/lista.html', {'filmes': filmes})
